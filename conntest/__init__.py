55730d7997c2c2ac3eea8735678de8a640e7c0fc
Philipp Schmitt <philipp@schmitt.co>
2016-10-04 08:19:19 +0200

    Add opsview_connection to scope

--- conntest/__init__.py
+++ conntest/__init__.py
@@ -2,3 +2,4 @@ from conntest import ssh_connection
 from conntest import rdp_connection
 from conntest import vnc_connection
 from conntest import vcenter_connection
+from conntest import opsview_connection
