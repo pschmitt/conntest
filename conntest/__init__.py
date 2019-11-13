978c8432f11c35308b13d8cf89768d2bdaf0ca73
Philipp Schmitt <philipp@schmitt.co>
2019-11-13 09:47:05 +0100

    Version 2.0.0
    - Drop opsview, VNC and RDP support
    - Python 3

--- conntest/__init__.py
+++ conntest/__init__.py
@@ -1,5 +1,1 @@
-from conntest import ssh_connection
-from conntest import rdp_connection
-from conntest import vnc_connection
-from conntest import vcenter_connection
-from conntest import opsview_connection
+from conntest import *
--- setup.py
+++ setup.py
@@ -2,17 +2,15 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name='conntest',
-    version='1.2.1',
-    license='GPL3',
-    description='Try to connect to a host via SSH/RDP/VNC/VMware vCenter auth etc',
-    long_description=open('README.rst').read(),
-    author='Philipp Schmitt',
-    author_email='philipp@schmitt.co',
-    url='https://github.com/pschmitt/conntest',
+    name="conntest",
+    version="2.0.0",
+    license="GPL3",
+    description="Try to connect to a host via SSH/RDP/VNC/VMware vCenter auth etc",
+    long_description=open("README.rst").read(),
+    author="Philipp Schmitt",
+    author_email="philipp@schmitt.co",
+    url="https://github.com/pschmitt/conntest",
     packages=find_packages(),
-    install_requires=['paramiko', 'rdpy', 'shortmomi', 'opsview'],
-    entry_points={
-        'console_scripts': ['conntest=conntest.conntest:main']
-    }
+    install_requires=["paramiko", "shortmomi"],
+    entry_points={"console_scripts": ["conntest=conntest.conntest:main"]},
 )
